b57aaa94330287d8d3b204fcbd10d63036fa7a10
DenisStenklerTrailer <77147548+DenisStenklerTrailer@users.noreply.github.com>
2023-02-02 07:41:53 +0100

    requirements.txt

--- web.py
+++ web.py
@@ -3,14 +3,16 @@ import functions
 
 todos = functions.ReadingTodos()
 
+
 def add_todo():
-    todo = st.session_state["new_todo"]
-    todos.append(todo + "\n")
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
     functions.WritingTodos(todos)
 
+
 st.title("My Todo App")
-st.subheader("This is my todo app")
-st.write("This app is cool")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity.")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -20,8 +22,9 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="",placeholder="Enter a todo here...",
-              on_change=add_todo, key="new_todo")
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
+
 
 # To je v zelo veliko pomoč ker nam izpisuje podatke
 #st.session_state
